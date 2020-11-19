7b976d4f3a57caa346c15f5613bf94b5fbc3741b
A7mooz <57980136+A7mooz@users.noreply.github.com>
2020-11-19 13:07:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'Nzc4ODgyMjkxMTkwNTk1NjA0.X7YcqQ.W9TNT9op9o0eiiZWkTCeco-WijM',    
+    TOKEN: '',    
     YT_API_KEY: 'api ', 
     prefix: '.',
     devs: ['738864122363379732']
